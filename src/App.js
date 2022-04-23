5f8104d6c2a22f735f1adcc134b6822e06f5bbb3
Luki Nguyen <nguyenhien236259@gmail.com>
2022-04-23 09:04:39 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Pannellum } from "pannellum-react";
+import myImage from "./images/milan.jpg";
+
+import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Pannellum
+        width="100%"
+        height="500px"
+        image={myImage}
+        pitch={10}
+        yaw={180}
+        hfov={110}
+        autoLoad
+        showZoomCtrl={false}
+        onLoad={() => {
+          console.log("panorama loaded");
+        }}
+      >
+        <Pannellum.Hotspot
+          type="custom"
+          pitch={31}
+          yaw={150}
+          handleClick={(evt, name) => console.log(name)}
+          name="hs1"
+        />
+      </Pannellum>
     </div>
   );
 }
 
-export default App;
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Pannellum } from "pannellum-react"; 
+import thumb from "./images/thumbnail.jpg"; 
+ import './App.css'
+function App() {
+  return (
+    <div className="App">
+      <Pannellum
+        width="100vw"
+        height="100vh"
+        haov={360}
+        vaov={180}
+        image={thumb}
+        pitch={10}
+        yaw={110} 
+        vOffsect={10}
+        hfov={110}
+        autoLoad
+        showZoomCtrl={false}
+        onLoad={() => {
+          console.log("panorama loaded");
+        }}
+      >
+        <Pannellum.Hotspot
+          type="custom"
+          pitch={3}
+          yaw={50}
+          xaw={60}
+          handleClick={(evt, name) => alert("Đây là lối đi lên cầu thang!!!!!")}
+          name="hs1"
+        />
+        <Pannellum.Hotspot
+          type="custom"
+          pitch={3}
+          yaw={-70}
+          xaw={40}
+          handleClick={(evt, name) => console.log(name)}
+          name="hs1"
+        />
+      </Pannellum>
+    </div>
+  );
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
